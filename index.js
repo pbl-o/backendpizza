3594939ca90a397fa668d8d9e1c4ad795a39d5fc
pblo <pdiazarenas541@gmail.com>
2026-02-11 17:23:34 +0000

    adding route at index.js

--- index.js
+++ index.js
@@ -18,6 +18,15 @@ app.use((_, res) => {
   res.status(404).json({ error: "Not Found" });
 });
 
+// Landing (for informationla purposes)
+app.get('/', ()=>{
+  try {
+    res.status(200).json({message: "Page working, using a temporary DB"})
+  } catch (error) {
+    res.status(500).json({message: "Server error"})
+  }
+})
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
