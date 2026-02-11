e218152ad507a3c9a24ac7b75a083bf36f9488e9
pblo <pdiazarenas541@gmail.com>
2026-02-11 17:29:07 +0000

    route'/' at index.js edit

--- index.js
+++ index.js
@@ -11,13 +11,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/auth", authRoute);
-app.use("/api/pizzas", pizzaRoute);
-app.use("/api/checkouts", checkoutRoute);
-app.use((_, res) => {
-  res.status(404).json({ error: "Not Found" });
-});
-
 // Landing (for informationla purposes)
 app.get('/', ()=>{
   try {
@@ -26,6 +19,15 @@ app.get('/', ()=>{
     res.status(500).json({message: "Server error"})
   }
 })
+
+app.use("/api/auth", authRoute);
+app.use("/api/pizzas", pizzaRoute);
+app.use("/api/checkouts", checkoutRoute);
+app.use((_, res) => {
+  res.status(404).json({ error: "Not Found" });
+});
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
